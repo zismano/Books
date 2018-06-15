498eacb3c9b80c037a3d0b1ff6ed51c120bf365e
Ofir Zisman <zismanofir@gmail.com>
2018-06-15 10:28:18 -0700

    add BookList component and query

---
+++ client/src/components/BookList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {gql} from 'apollo-boost';
+import {graphql} from 'react-apollo';
+
+// gql and then right afterwards the query syntax like in graphiql
+const getBooksQuery = gql`
+  {
+  	books {
+  		name
+  		id
+  	}
+  }
+`
+
+class BookList extends Component {
+  render() {
+  	console.log(this.props);
+    return (
+      <div>
+        <ul id="book-list">
+          <li>Book name</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default graphql(getBooksQuery)(BookList);
