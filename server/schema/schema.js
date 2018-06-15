3e8b3ba2fab5a284deff280ea18e46325534bfc2
Ofir Zisman <zismanofir@gmail.com>
2018-06-15 08:54:10 -0700

    add addBook mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -110,6 +110,22 @@ const Mutation = new GraphQLObjectType({
 				});	// author is a new instance of Author from mongoose
 				return author.save();	// save author to mongodb and then return it
 			}
+		},
+		addBook: {
+			type: BookType,
+			args: {
+				name: {type: GraphQLString},
+				genre: {type: GraphQLString},
+				authorId: {type:GraphQLID}
+			},
+			resolve(parent, args) {
+				let book = new Book({
+					name: args.name,
+					genre: args.genre,
+					authorId: args.authorId
+				});
+				return book.save();
+			}
 		}
 	}
 })
