2c1cb2043409edb6dc3fd67654a463e0d433020b
Ofir Zisman <zismanofir@gmail.com>
2018-06-14 19:10:22 -0700

    add connection to mongodb

--- server/app.js
+++ server/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// connect to mlab database
+mongoose.connect("mongodb://ofir:test123@ds035290.mlab.com:35290/gql-ninja");
+mongoose.connection.once('open', () => {
+	console.log('connected to database');
+})
+
 app.use('/graphql', graphqlHTTP({
-	schema
+	schema,
+	graphiql:true
 }));
 
 app.listen(4000, () => {
