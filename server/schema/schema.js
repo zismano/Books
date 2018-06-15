032a990e49398e767925a259a6fc9f196075820d
Ofir Zisman <zismanofir@gmail.com>
2018-06-15 08:40:07 -0700

    add addAuthor mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require('graphql');
 const _ = require('lodash');
+const Book = require('../models/book');
+const Author = require('../models/author');
 
 const { 
 	GraphQLObjectType, 
@@ -11,20 +13,20 @@ const {
 } = graphql;
 
 // dummy data
-var books = [
-	{name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
-	{name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
-	{name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},	
-	{name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2'},
-	{name: 'The Color of Magic', genre: 'Fantasy', id: '5', authorId: '3'},
-	{name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3'},
-];
+// var books = [
+// 	{name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
+// 	{name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
+// 	{name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},	
+// 	{name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2'},
+// 	{name: 'The Color of Magic', genre: 'Fantasy', id: '5', authorId: '3'},
+// 	{name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3'},
+// ];
 
-var authors = [
-	{name: 'Patrick Rothfuss', age: 44, id: '1'},
-	{name: 'Brandon Sanderson', age: 42, id: '2'},
-	{name: 'Terry Pratchett', age: 66, id: '3'},	
-];
+// var authors = [
+// 	{name: 'Patrick Rothfuss', age: 44, id: '1'},
+// 	{name: 'Brandon Sanderson', age: 42, id: '2'},
+// 	{name: 'Terry Pratchett', age: 66, id: '3'},	
+// ];
 
 const BookType = new GraphQLObjectType({
 	name: 'Book',
@@ -36,7 +38,7 @@ const BookType = new GraphQLObjectType({
 			type: AuthorType,
 			resolve(parent, args) {	// use of resolve to get related data in graphql. Parent is a book
 				console.log(parent);
-				return _.find(authors, {id: parent.authorId})
+			//	return _.find(authors, {id: parent.authorId})
 			}
 		}
 	})
@@ -51,7 +53,7 @@ const AuthorType = new GraphQLObjectType({
 		books: {
 			type: new GraphQLList(BookType), // to get array of books
 			resolve(parent, args) {
-				return _.filter(books, {authorId: parent.id});
+			//	return _.filter(books, {authorId: parent.id});
 			}
 		}
 	})
@@ -67,38 +69,61 @@ const RootQuery = new GraphQLObjectType({
 			resolve(parent, args) {
 				// code to get data from db/other source
 				console.log(typeof args.id);
-				return _.find(books, {id: args.id});
+			//	return _.find(books, {id: args.id});
 			}
 		},
 		author: {
 			type: AuthorType,
 			args: { id: {type: GraphQLID}},
 			resolve(parent, args) {
-				return _.find(authors, {id: args.id});
+			//	return _.find(authors, {id: args.id});
 			}
 		},
 		books: {
 			type: new GraphQLList(BookType),
 			resolve(parent, args) {
-				return books;
+			//	return books;
 			}
 		},
 		authors: {
 			type: new GraphQLList(AuthorType),
 			resolve(parent, args) {
-				return authors;
+			//	return authors;
 			}
 		}
 	}
 });
 
-/* 
-  book(id: "2") {
-	name
-	genre
-  }
-*/
+const Mutation = new GraphQLObjectType({
+	name: 'Mutation',
+	fields: {
+		addAuthor: {
+			type: AuthorType,
+			args: {
+				name: {type: GraphQLString},
+				age: {type: GraphQLInt}
+			},
+			resolve(parent, args) {
+				let author = new Author({
+					name: args.name,
+					age: args.age
+				});	// author is a new instance of Author from mongoose
+				return author.save();	// save author to mongodb and then return it
+			}
+		}
+	}
+})
 
+/*
+	// add author Ofir, 30 and after creating it, what data would you like to get back (name, age)
+  mutation {
+	addAuthor(name: 'Ofir', age: 30) {
+		name
+		age
+	}
+  }
+ */
 module.exports = new GraphQLSchema({
-	query: RootQuery
+	query: RootQuery,
+	mutation: Mutation
 });
