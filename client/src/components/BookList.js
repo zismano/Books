150ed8b368553a44ed3f31cbee5124c63e5e4f36
Ofir Zisman <zismanofir@gmail.com>
2018-06-15 12:26:42 -0700

    add event listener, and BookDetails component

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 import {graphql} from 'react-apollo';
 import {getBooksQuery} from '../queries/queries';
 
+// components
+import BookDetails from './BookDetails';
+
 class BookList extends Component {
+  constructor(props) {
+  	super(props);
+  	this.state = {
+  		selected: null
+  	}
+  }
   displayBooks() {
   	var data = this.props.data;
   	if (data.loading) {
@@ -10,7 +19,7 @@ class BookList extends Component {
   	} else {
   		return data.books.map(book => {
   			return (
-  				<li key={book.id}>{book.name}</li>
+  				<li key={book.id} onClick={(e) => {this.setState({selected: book.id})}}>{book.name}</li>
   			);
   		})
   	}
@@ -22,6 +31,7 @@ class BookList extends Component {
         <ul id="book-list">
           {this.displayBooks()}
         </ul>
+        <BookDetails bookId={this.state.selected}/>
       </div>
     );
   }
